0c84771a10318a46377ec8cb9b830d6597256dd6
domHenin <domhenin.517@gmail.com>
2019-02-12 14:27:58 -0500

    was able to add a method that will allow user to input own information

--- src/AccountOwner.java
+++ src/AccountOwner.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 public class AccountOwner {
+    Scanner input = new Scanner(System.in);
     private String personName;
     private double personAcctNum;
 
-    public AccountOwner(String personName, double personAcctNum) {
-        this.personName = personName;
-        this.personAcctNum = personAcctNum;
-    }
+//    public AccountOwner(String personName, double personAcctNum) {
+//        this.personName = personName;
+//        this.personAcctNum = personAcctNum;
+//    }
+
+
+    public void setPersonName(String personName) { this.personName = personName; }
+    public void setPersonAcctNum(double personAcctNum) { this.personAcctNum = personAcctNum; }
 
     public String getPersonName() { return personName; }
     public double getPersonAcctNum() { return personAcctNum; }
+
+    public void changePersonName(/*String newPersonName*/) {
+        String newPersonName;
+
+        System.out.println("Enter Name on Account: ");
+        newPersonName = input.next();
+
+        personName = newPersonName;
+    }
+
+    public void changePersonAccountNumber() {
+        int newAccountNumber;
+
+        System.out.println("Enter Account Number for "+getPersonName()+": ");
+        newAccountNumber = input.nextInt();
+
+        personAcctNum = newAccountNumber;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
-        AccountOwner person = new AccountOwner("Goku", 123);
+        AccountOwner person = new AccountOwner();
+        AccountOwner person1 = new AccountOwner();
         bankAccount.setBalance(600);
+
+        person.setPersonName("Goku");
+        person.setPersonAcctNum(123);
 //test bankAccount1.setBalance(200);
+
+//  Bank Account Code:
         System.out.println("Initial Balance: "+bankAccount.getBalance());
 //test System.out.println("Initial Balance: "+bankAccount1.getBalance());
         System.out.println("Deposit Money: "+bankAccount.depositBalance(25));
@@ -11,9 +17,18 @@ public class Main {
         System.out.println("Withdraw Money: "+bankAccount.withdrawBalance(10));
 //test BankAccount bankAccount1 = new BankAccount();
         System.out.println("Balance: "+bankAccount.changeBalance());
+
+//  Account Owner Code:
         System.out.println("Name on Account: "+person.getPersonName());
         System.out.println("Account Number: "+person.getPersonAcctNum()+ " for "+person.getPersonName());
+
+        person1.changePersonName();
+        person1.changePersonAccountNumber();
+        System.out.println(person1.getPersonName());
+        System.out.println(person1.getPersonAcctNum());
 // System.out.println("New Balance: "+bankAccount.withdrawBalance(50));
+
+
     }
 }
 //TODO: (check)
